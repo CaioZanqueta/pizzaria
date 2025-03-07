4b6e6483bcd2e1947ed8ef7e1e8a2bbc8da57538
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2025-03-07 12:00:31 -0300

    Pizzaria Project

---
+++ backend/src/routes.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import multer from "multer";
+
+// -- USERS CONTROLLERS -- //
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+// -- CATEGORIES CONTROLLERS -- //
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+
+// -- PRODUCTS CONTROLLERS -- //
+import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+
+// -- ORDERS CONTROLLERS -- //
+import { CreateOrderController } from "./controllers/order/CreateOrderController"; 
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController"; 
+import { AddItemController } from "./controllers/order/AddItemController"; 
+
+// -- MIDDLEWARE -- //
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer"
+
+const router = Router()
+const upload = multer(uploadConfig.upload("./tmp"))
+
+// -- ROTAS USERS -- //
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// -- ROTAS CATEGORIES -- //
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// -- ROTAS PRODUCTS -- //
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// -- ROTAS ORDERS -- //
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+
+export { router }
