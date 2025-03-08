aaf549363c37b1c7aba2573fe7caa154694edff9
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2025-03-08 01:46:58 -0300

    Backend Done

--- backend/src/controllers/order/ListOrdersController.ts
+++ backend/src/controllers/order/ListOrdersController.ts
@@ -4,6 +4,10 @@ import { ListOrdersService } from "../../services/order/ListOrdersService";
 class ListOrdersController {
   async handle(req: Request, res: Response) {
     const listOrders = new ListOrdersService();
+
+    const orders = await listOrders.execute()
+
+    return res.json(orders)
   }
 }
 
