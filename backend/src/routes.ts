45878faa6f8fc546aff26daaca55e165e74c5377
Caio Zanqueta <caiozan2001@hotmail.com>
2025-03-08 00:40:26 -0300

    Update

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,6 +18,8 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 import { CreateOrderController } from "./controllers/order/CreateOrderController"; 
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController"; 
 import { AddItemController } from "./controllers/order/AddItemController"; 
+import { RemoveItemController } from "./controllers/order/RemoveItemController"; 
+import { SendOrderController } from "./controllers/order/SendOrderController"; 
 
 // -- MIDDLEWARE -- //
 import { isAuthenticated } from "./middlewares/isAuthenticated";
@@ -43,5 +45,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router }
