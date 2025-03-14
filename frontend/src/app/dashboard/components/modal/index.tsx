6389dbe07ece560a2a56f0d9cf7e893fd121a803
Caio Zanqueta <81552311+CaioZanqueta@users.noreply.github.com>
2025-03-14 17:59:32 -0300

    Update modal

--- frontend/src/app/dashboard/components/modal/index.tsx
+++ frontend/src/app/dashboard/components/modal/index.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import styles from './styles.module.scss'
 import { X } from 'lucide-react'
+import { use } from 'react'
+import { OrderContext } from '@/providers/order'
 
 export function ModalOrder() {
+  const { onRequestClose } = use(OrderContext)
+
   return (
     <dialog className={styles.dialogContainer}>
       <section className={styles.dialogContent}>
-        <button className={styles.dialogBack}>
+        <button className={styles.dialogBack} onClick={onRequestClose}>
           <X size={40} color='#FF3f4b' />
         </button>
 
